4f2a332b817e94bbf6bdff30ef5e2e73bbc4ff83
Jared Hodgkins <PhusionDev@gmail.com>
2022-10-05 08:25:57 -0400

    26. Using Buttons

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,7 @@ import {
   useScrollTrigger,
   Tabs,
   Tab,
+  Button,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
@@ -38,6 +39,13 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 10,
     marginLeft: '25px',
   },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px',
+  },
 }));
 
 const Header = (props) => {
@@ -55,6 +63,13 @@ const Header = (props) => {
               <Tab className={classes.tab} label='About Us' />
               <Tab className={classes.tab} label='Contact Us' />
             </Tabs>
+            <Button
+              variant='contained'
+              color='secondary'
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
